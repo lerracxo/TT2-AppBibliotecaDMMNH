d2a85cdd6da5f6fa0d38ed36742726c4d5a07331
Oscar Omar Santoyo Martínez <lerracxo@gmail.com>
2014-02-19 09:21:17 -0600

    Agregadas las cargas iniciales de los tipos de archivos.

--- DataBase/Cargas inciales.sql
+++ DataBase/Cargas inciales.sql
@@ -9,3 +9,7 @@ INSERT INTO usuario VALUES(2,1,0,"revi",sha1('revi'),"Revisor","AP Revisor","AM 
 -- Tabla c_tipo_usuario
 INSERT INTO c_tipo_usuario VALUES(1,1,"Administrador","Administrador principal del sistema.");
 INSERT INTO c_tipo_usuario VALUES(2,1,"Revisor","Validador de información del sistema.");
+
+-- Tipo de usuarios
+INSERT INTO c_tipo_usuario VALUES (1,"Administrador","Administrador del sistema");
+INSERT INTO c_tipo_usuario VALUES (2,"Revisor","Revisor del sistema");
