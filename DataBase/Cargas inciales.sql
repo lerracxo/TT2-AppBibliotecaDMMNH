058794b26144a04ecb6353d814b4382584ee1605
Oscar Omar Santoyo Martínez <lerracxo@gmail.com>
2014-03-10 10:24:24 -0600

    Modificación de las cargas iniciales de informacion.

--- DataBase/Cargas inciales.sql
+++ DataBase/Cargas inciales.sql
@@ -4,12 +4,12 @@
 INSERT INTO usuario VALUES(1,1,0,"admin",sha1('admin'),"Administrador","AP Administrador","AM Administrador","admin@hotmail.com",0,0,0,0,0,0);
 INSERT INTO usuario VALUES(2,1,0,"revi",sha1('revi'),"Revisor","AP Revisor","AM Revisor","revi@hotmail.com",0,0,0,0,0,0);
 
-
-
 -- Tabla c_tipo_usuario
-INSERT INTO c_tipo_usuario VALUES(1,1,"Administrador","Administrador principal del sistema.");
-INSERT INTO c_tipo_usuario VALUES(2,1,"Revisor","Validador de información del sistema.");
+INSERT INTO c_tipo_usuario VALUES(1,"Administrador","Administrador principal del sistema.");
+INSERT INTO c_tipo_usuario VALUES(2,"Revisor","Validador de información del sistema.");
 
--- Tipo de usuarios
-INSERT INTO c_tipo_usuario VALUES (1,"Administrador","Administrador del sistema");
-INSERT INTO c_tipo_usuario VALUES (2,"Revisor","Revisor del sistema");
+-- Tipos de visitantes
+INSERT INTO cat_ocupaciones VALUES (1,"Estudiante");
+INSERT INTO cat_ocupaciones VALUES (2,"Profesor");
+INSERT INTO cat_ocupaciones VALUES (3,"Turista");
+INSERT INTO cat_ocupaciones VALUES (4,"Visitante casual");
